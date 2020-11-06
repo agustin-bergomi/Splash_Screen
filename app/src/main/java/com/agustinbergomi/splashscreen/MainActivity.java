6fb503fe67dca93286b9f6651da03b492754a89a
Agustin Bergomi <agustin.bergomi@gmail.com>
2020-11-06 15:41:15 -0300

    comentarios

--- app/src/main/java/com/agustinbergomi/splashscreen/MainActivity.java
+++ app/src/main/java/com/agustinbergomi/splashscreen/MainActivity.java
@@ -11,10 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //creamos un nuevo thread porque es una operacion que dura varios segundos en la GUI
         Thread splash = new Thread() {
             public void run() {
                 try {
                     sleep(3 * 1000);
+                    //se muestra 3 segundos la activity main con la imagen.
+                    //cuando termina se inicia start activity con el intent que lleva a la HomePage.
                     Intent i = new Intent(getBaseContext(), HomePage.class);
                     startActivity(i);
                     finish();
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+        //se inicializa el thread
         splash.start();
     }
 }
